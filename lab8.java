32b70a53012be7929f06f0cab4e9268e6babd7ff
1122coder <malikmuhammadkashif414@gmail.com>
2022-05-30 20:47:12 +0500

    simple code added

---
+++ lab8.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kashifpackage;
+
+/**
+ *
+ * @author Kashif Saeed
+ */
+public class lab8 {
+    public static void main(String[] args){
+        System.out.println("Git demo lab 8");
+    }
+}
